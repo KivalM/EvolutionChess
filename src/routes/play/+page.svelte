17684b618f4f2495842d57890b39b95dddb48e55
Kival Mahadew <kivalm@protonmail.com>
2022-11-10 22:54:15 +0200

    basic structure

---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>This is where we will have the game</h1>
+</div>
