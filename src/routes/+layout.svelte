de976e6017cdf86c1b0c6f9a395dac2865354cc2
Kival Mahadew <kivalm@protonmail.com>
2022-11-10 23:03:17 +0200

    add temp for about

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,7 @@
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<footer />
 </div>
 
 <style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,18 +4,12 @@
 </svelte:head>
 
 <div>
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+	<h1>EvolvedChess</h1>
 </div>
+
+<style>
+	h1 {
+		color: navy;
+		font-size: 50px;
+	}
+</style>
