2a8b29039c17cbdc4e9dbf2964ed5241dfde0095
Kival Mahadew <kivalm@protonmail.com>
2022-11-11 21:24:46 +0200

    about page stuff

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,45 +3,38 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div>
-	<h1>Evolved Chess</h1>
-	<h2>A new world of chess awaits! Are you ready?!</h2>
+<section>
+	<!-- align center -->
+	<div class="flex justify-center">
+		<div class="container">
+			<h1 class="text-4xl font-bold text-green-500 ">Evolved Chess</h1>
+			<h2 class="text-2xl font-bold ">A new world of chess awaits! Are you ready?!</h2>
+			<p class="my-4 text-lg text-gray-500">
+				Chess got a whole lot interesting. As time goes on everything adapts and evolves so why not
+				this amazing game. We all know how intense chess battles are, now with a twist how will all
+				you chess grandmasters handle this?
+			</p>
+			<h3 class="font-bold">What's new :</h3>
+			<p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
+				Imagine being a pawn fighting for your kingdom, with despair smacking against your face in
+				the midst of battle. No where to go but foward towards victory. With your heart pumping and
+				adrenaline rushing through your body. You slay your enemies one by one. You look down at
+				your hands and feel like hero in your own story.
+			</p>
+			<ul class="space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+				<li>Pawns will ....</li>
+				<li>At least one lowercase character</li>
+				<li>Inclusion of at least one special character, e.g., ! @ # ?</li>
+			</ul>
+
+			<!-- <h4>MAIN CHARACTER :</h4>
 	<p>
-		Chess got a whole lot interesting. As time goes on everything adapts and evolves so why not this
-		amazing game. We all know how intense chess battles are, now with a twist how will all you chess
-		grandmasters handle this?
-	</p>
-	 <h3>What's new :</h3>
-	 <p>
-		Imagine being a pawn fighting for your kingdom, with despair smacking against your face in the midst
-		of battle. No where to go but foward towards victory. With your heart pumping and adrenaline
-		rushing through your body. You slay your enemies one by one. You look down at your hands and feel
-		like hero in your own story.
-	 </p>
-	 <h4>
-		MAIN CHARACTER :
-	  </h4>
-	  <p>
-       An ability exclusive to pawns. You can evolve into other chess pieces after taking out a certain 
-	   number of foes. 
-	  </p>
-</div>
+		An ability exclusive to pawns. You can evolve into other chess pieces after taking out a certain
+		number of foes.
+	</p> -->
+		</div>
+	</div>
+</section>
 
 <style>
-	h1 {
-		color: rgb(245, 6, 205);
-		font-size: 50px;
-	} h2 {
-		color: rgb(245, 6, 205);
-		font-size: 40px;
-	} p {
-		color: navy;
-		font-size: 15px;
-	} h3 {
-		color: rgb(245, 6, 205);
-		font-size: 20px;
-	} h4 {
-		color: rgb(245, 6, 205);
-		font-size: 16px;
-	}
 </style>
