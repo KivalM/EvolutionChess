ad063103f71607318f6324acd3680b9976aa9853
Seevan Ponnan <seevanponnan@gmail.com>
2022-11-11 20:39:00 +0200

    Home

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,19 +8,40 @@
 	<h2>A new world of chess awaits! Are you ready?!</h2>
 	<p>
 		Chess got a whole lot interesting. As time goes on everything adapts and evolves so why not this
-		amazing game.
+		amazing game. We all know how intense chess battles are, now with a twist how will all you chess
+		grandmasters handle this?
 	</p>
+	 <h3>What's new :</h3>
+	 <p>
+		Imagine being a pawn fighting for your kingdom, with despair smacking against your face in the midst
+		of battle. No where to go but foward towards victory. With your heart pumping and adrenaline
+		rushing through your body. You slay your enemies one by one. You look down at your hands and feel
+		like hero in your own story.
+	 </p>
+	 <h4>
+		MAIN CHARACTER :
+	  </h4>
+	  <p>
+       An ability exclusive to pawns. You can evolve into other chess pieces after taking out a certain 
+	   number of foes. 
+	  </p>
 </div>
 
 <style>
 	h1 {
-		color: navy;
+		color: rgb(245, 6, 205);
 		font-size: 50px;
 	} h2 {
 		color: rgb(245, 6, 205);
 		font-size: 40px;
 	} p {
-		color: blue;
+		color: navy;
 		font-size: 15px;
+	} h3 {
+		color: rgb(245, 6, 205);
+		font-size: 20px;
+	} h4 {
+		color: rgb(245, 6, 205);
+		font-size: 16px;
 	}
 </style>
