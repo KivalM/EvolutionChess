1a1a24ccf360b1d74c9ad3c5b9f69f2bd078fd5c
Seevan Ponnan <seevanponnan@gmail.com>
2022-11-11 22:25:39 +0200

    Header change

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,7 +7,7 @@
 	<!-- align center -->
 	<div class="flex justify-center">
 		<div class="container">
-			<h1 class="text-4xl font-bold text-green-500 ">Evolved Chess</h1>
+			<h1 class="text-4xl font-bold text-blue-700 ">Evolved Chess</h1>
 			<h2 class="text-2xl font-bold ">A new world of chess awaits! Are you ready?!</h2>
 			<p class="my-4 text-lg text-gray-500">
 				Chess got a whole lot interesting. As time goes on everything adapts and evolves so why not
@@ -19,19 +19,20 @@
 				Imagine being a pawn fighting for your kingdom, with despair smacking against your face in
 				the midst of battle. No where to go but foward towards victory. With your heart pumping and
 				adrenaline rushing through your body. You slay your enemies one by one. You look down at
-				your hands and feel like hero in your own story.
+				your hands and feel like hero in your own story. Now is your chance to be something more!!!
 			</p>
 			<ul class="space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
-				<li>Pawns will ....</li>
-				<li>At least one lowercase character</li>
-				<li>Inclusion of at least one special character, e.g., ! @ # ?</li>
+				<li>Pawns can now evolve!</li>
+				<li>Every chess piece is assigned a certain number of points</li>
+				<ol class="pl-5 mt-2 space-y-1 list-decimal list-inside">
+				   <li> Queens are worth the most with 7 points </li>
+				   <li>Rooks are worth 5 points</li> 
+                   <li>Bishops and knights are worth 3 points</li>    
+				   <li> Pawns are worth 1 point</li> 
+				</ol>
+				<li>Each time a pawn eliminates one of these pieces it evolves</li>
 			</ul>
-
-			<!-- <h4>MAIN CHARACTER :</h4>
-	<p>
-		An ability exclusive to pawns. You can evolve into other chess pieces after taking out a certain
-		number of foes.
-	</p> -->
+            <h4 class="font-bold"> How it works :</h4>
 		</div>
 	</div>
 </section>
