3cb9b6a6acbccbfd29d021c24604fe888349c16d
Kival Mahadew <kivalm@protonmail.com>
2024-07-24 15:35:49 +0200

    chore: update svelte.config.js for production deployment

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/ukzn_hackathon" : "",
+			base: process.env.NODE_ENV === "production" ? "/EvolutionChess" : "",
 		},
 		appDir: 'internal',
 	}
